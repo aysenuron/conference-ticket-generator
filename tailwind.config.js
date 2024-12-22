097c7c23e5ef1ec67a9cc0d967c64d5439bbd36b
Aysenur <onaranays@gmail.com>
2024-12-22 15:52:47 +0300

    Create mobile view of form.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inconsolata: ['Inconsolata', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
